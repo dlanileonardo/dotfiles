240fb52202f083ade7fc2eebd6a93867b661b88b
Dlani Mendes <dlanileonardo@gmail.com>
2022-06-14 02:03:58 -0300

    Maravilha

--- lvim/config.lua
+++ lvim/config.lua
@@ -613,6 +613,48 @@ lvim.builtin.which_key.mappings["t"] = {
   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
 
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _LAZYGIT_TOGGLE()
+  lazygit:toggle()
+end
+
+local node = Terminal:new({ cmd = "node", hidden = true })
+
+function _NODE_TOGGLE()
+  node:toggle()
+end
+
+local ctop = Terminal:new({ cmd = "ctop", hidden = true })
+
+function _CTOP_TOGGLE()
+  ctop:toggle()
+end
+
+local htop = Terminal:new({ cmd = "htop", hidden = true })
+
+function _HTOP_TOGGLE()
+  htop:toggle()
+end
+
+local python = Terminal:new({ cmd = "python", hidden = true })
+
+function _PYTHON_TOGGLE()
+  python:toggle()
+end
+
+lvim.builtin.which_key.mappings["a"] = {
+  name = "Terminal",
+  n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+  u = { "<cmd>lua _CTOP_TOGGLE()<cr>", "CTOP" },
+  t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+  p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+  f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+  h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+  v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+}
+
 local command_center = require("command_center")
 -- local noremap = { noremap = true }
 -- local silent_noremap = { noremap = true, silent = true }
